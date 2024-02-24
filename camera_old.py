cd0c1b540ae4e2f96303d4137524a6003e30a7ac
rolson24 <raif.t.olson.24@dartmouth.edu>
2024-02-24 15:27:22 -0500

    Added comments to main, TODOs in others

---
+++ camera_old.py
@@ -0,0 +1,191 @@
+
+from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder, Quality
+from picamera2.outputs import FileOutput
+import time
+import threading
+import io
+import os
+
+# need cv2 to check if the frame is too dark
+import cv2
+
+SEGMENT_LENGTH = 15 * 60 # 15 minutes * 60 seconds
+# SEGMENT_LENGTH = 10 # 10 seconds
+PREVIEW_FRAMERATE = 1.0
+RESOLUTION = (1280, 720)
+SWITCH_CHECK_INTERVAL = 5 # 60 seconds
+DARK_THRESHOLD = 50 # average pixel value below this is considered too dark
+
+day_cam = None
+day_cfg = None
+night_cam = None
+night_cfg = None
+use_night = True # True = night, False = day
+latest_frame_queue = None
+latest_frame = None
+last_camera_check = 0
+video_writer = None
+
+def update_frame():
+    print("Started update thread")
+    global PREVIEW_FRAMERATE, use_night, day_cam, night_cam, latest_frame_queue, latest_frame, video_writer
+    frame = None
+    last_frame_time = 0
+    while True:
+        data = io.BytesIO()
+        if use_night:
+            night_cam.capture_file(data, format='jpeg')
+            latest_frame = night_cam.capture_array()
+        else:
+            day_cam.capture_file(data, format='jpeg')
+            latest_frame = day_cam.capture_array()
+            if video_writer is not None and latest_frame is not None:
+                video_writer.write(cv2.cvtColor(latest_frame, cv2.COLOR_RGB2BGR))
+        frame = data.getvalue()
+        latest_frame_queue.put(frame)
+        time.sleep(1 / PREVIEW_FRAMERATE)
+
+def frame_is_dark(frame):
+#    # do some analysis to see if the frame is too dark
+#     cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     # check if the average pixel value is below a certain threshold
+#     avg_pixel = cv2.mean(frame)
+#     # Dark camera is IR, so only use green channel for average pixel value
+#     avg_pixel = avg_pixel[1]
+#     # print(F"Average: {avg_pixel} | Dark threshold: {DARK_THRESHOLD}")
+#     if avg_pixel < DARK_THRESHOLD:
+#         return True
+#     else:
+#         return False
+    return True
+
+def camera_worker(preview_framerate, queue, state):
+    print(F"Starting camera worker with preview framerate: {preview_framerate}")
+    # Initialize the camera and worker process
+    global RESOLUTION, PREVIEW_FRAMERATE, day_cam, day_cfg, latest_frame_queue, encoder, night_cam, night_cfg, latest_frame, use_night, last_camera_check, video_writer
+    PREVIEW_FRAMERATE = preview_framerate
+    latest_frame_queue = queue
+    # day_cam = Picamera2(1) # The USB camera?
+    night_cam = Picamera2(0) # the CSI2 camera
+    print("Created camera instances")
+    # day_cfg = day_cam.create_video_configuration(main={"size": RESOLUTION})
+    night_cfg = night_cam.create_video_configuration(main={"size": RESOLUTION})
+    print("Created camera configurations")
+    # day_cam.configure(day_cfg)
+    night_cam.configure(night_cfg)
+    print("Configured cameras")
+    encoder = H264Encoder()
+    print("Created encoder")
+    # day_cam.start()
+    night_cam.start()
+    print("Started cameras")
+
+    # Start the background thread to update the latest preview frame
+    update_thread = threading.Thread(target=update_frame)
+    update_thread.daemon = True
+    update_thread.start()
+
+    segments = []
+    segment_count = 1
+    segment_start_time = None
+    should_combine = False
+    last_camera = use_night
+    while True:
+        # Check if the camera should be switched
+        # if time.time() - last_camera_check >= SWITCH_CHECK_INTERVAL and latest_frame is not None:
+            # Check latest_frame to see if it's too dark
+            # if frame_is_dark(latest_frame):
+            #     use_night = True
+            # else:
+            #     use_night = False
+            # last_camera_check = time.time()
+
+        # if last_camera != use_night and state['recording']: # Camera has switched
+        #     if use_night:
+        #         print("Switching to night camera")
+        #         # day_cam.stop_encoder()
+        #         video_writer.release()
+        #         output = FileOutput(f"{state['recording_directory']}/video_{segment_count}.mp4")
+        #         night_cam.start_encoder(encoder, output, quality=Quality.HIGH)
+        #     else:
+        #         print("Switching to day camera")
+        #         night_cam.stop_encoder()
+        #         output_file = f"{state['recording_directory']}/video_{segment_count}.mp4"
+        #         # output = FileOutput(f"{state['recording_directory']}/video_{segment_count}.mp4")
+        #         # day_cam.start_encoder(encoder, output, quality=Quality.HIGH)
+        #         video_writer = cv2.VideoWriter(output_file, cv2.VideoWriter_fourcc(*'mp4v'), PREVIEW_FRAMERATE, RESOLUTION)
+        #     segment_start_time = time.time()
+        #     last_camera = use_night
+
+        if state['recording'] and (time.time() - segment_start_time) >= SEGMENT_LENGTH:
+            print("Segment length reached")
+            # cam.stop_recording()
+            if use_night:
+                night_cam.stop_encoder()
+            else:
+                # day_cam.stop_encoder()
+                video_writer.release()
+            print("Encoder stopped")
+            segments.append(f"{state['recording_directory']}/video_{segment_count}.mp4")
+            segment_count += 1
+            output = FileOutput(f"{state['recording_directory']}/video_{segment_count}.mp4")
+            if use_night:
+                night_cam.start_encoder(encoder, output, quality=Quality.HIGH)
+            else:
+                # day_cam.start_encoder(encoder, output, quality=Quality.HIGH)
+                output_file = f"{state['recording_directory']}/video_{segment_count}.mp4"
+                video_writer = cv2.VideoWriter(output_file, cv2.VideoWriter_fourcc(*'mp4v'), PREVIEW_FRAMERATE, RESOLUTION)
+            segment_start_time = time.time()
+            print(F"Encoder started, recording to {state['recording_directory']}/video_{segment_count}.mp4")
+
+        if should_combine:
+            print("Combining segments")
+            # Combine segments using ffmpeg
+            # ffmpeg -i "concat:input1.mp4|input2.mp4|input3.mp4" -c copy output.mp4
+            state['combining'] = True
+            time.sleep(5) # Wait a few seconds for the last segment to write
+            os.system(F"ffmpeg -i 'concat:{'|'.join(segments)}' -c copy {state['recording_directory']}/video.mp4")
+            # Remove segments
+            # for segment in segments:
+            #     os.remove(segment)
+            segments = []
+            segment_count = 1
+            should_combine = False
+            state['recording_directory'] = None
+            print("Combined segments")
+            state['combining'] = False
+        
+        if state['should_record'] and not state['recording']:
+            # Wait for the recording directory to be set
+            while state['recording_directory'] is None:
+                time.sleep(0.1)
+            print("Starting recording")
+            # Directory for current recording session will have already been created
+            # Start recording first segment
+            output = FileOutput(f"{state['recording_directory']}/video_{segment_count}.mp4")
+            if use_night:
+                night_cam.start_encoder(encoder, output, quality=Quality.HIGH)
+            else:
+                # day_cam.start_encoder(encoder, output, quality=Quality.HIGH)
+                output_file = f"{state['recording_directory']}/video_{segment_count}.mp4"
+                video_writer = cv2.VideoWriter(output_file, cv2.VideoWriter_fourcc(*'mp4v'), PREVIEW_FRAMERATE, RESOLUTION)
+            state['recording_start_time'] = time.time()
+            segment_start_time = time.time()
+            print(F"Encoder started, recording to {state['recording_directory']}/video_{segment_count}.mp4")
+            state['recording'] = True
+        elif not state['should_record'] and state['recording']:
+            print("Stopping recording")
+            if use_night:
+                night_cam.stop_encoder()
+            else:
+                # day_cam.stop_encoder()
+                video_writer.release()
+            print("Encoder stopped")
+            segments.append(f"{state['recording_directory']}/video_{segment_count}.mp4")
+            state['recording'] = False
+            should_combine = True
+        time.sleep(0.1)
+
+if __name__ == "__main__":
+    print(F"This is the camera worker module, and should not be run directly.")
--- main.py
+++ main.py
@@ -1,5 +1,35 @@
-# Main file for recording device
-# Starts web server process and camera handler process
+"""
+Main python file for the Bike Walk Census Tool Recording Device
+Built for the purposes of our ENGS 89/90 Bike Walk Census Tool to record
+video at both daytime and nighttime using two separate cameras, one without
+an IR filter to capture infrared wavelengths. Nighttime video is meant to be
+paired with our infrared spotlight for active illumination purposes.
+
+This file controls all the GPIO and spins off separate threads
+to control the camera logic and web UI.
+
+The features of this program include:
+- The ability to turn the Pi off
+- The ability to start and stop the recording
+- The ability to mount and unmount the SD Card
+
+Expected Hardware:
+- Raspberry Pi 4 B
+- Andrei's custom PCB with 3 buttons, 3 LEDs
+- USB Flash Storage Device (64GB or more recommended)
+- One USB Camera (used as daytime, we chose ELP 5 MP OV5640 sensor)
+- One CSI 15 Pin Camera (used as nighttime camera, 5 MP OV5647 sensor w/o IR filter)
+
+Software Dependencies:
+- Picamera2
+- OpenCV
+- Flask
+
+Authors:
+- Andrei Gerashchenko
+- Raif Olson
+- Wending Wu
+"""
 
 import multiprocessing
 import time
@@ -7,33 +37,45 @@ import os
 import json
 import RPi.GPIO as GPIO
 
-import camera_test
+import camera
 import webui
 
+## CONSTANTS 
+### LED Pins
 POWER_LED_PIN = 18
 RECORDING_LED_PIN = 12
 MOUNT_LED_PIN = 13
+
+### BUTTON Pins
 POWER_BTN_PIN = 3
 RECORDING_BTN_PIN = 22
 MOUNT_BTN_PIN = 23
+
+## SETTINGS
 PREVIEW_FRAMERATE = 30.0
-RECORDING_MAIN_DIRECTORY = "/recordings"
 FAST_LED_BLINK_INTERVAL = 0.1
 SLOW_LED_BLINK_INTERVAL = 1.0
 DEBOUNCE_TIME = 0.25
-
+RECORDING_MAIN_DIRECTORY = "/recordings"
 
 def get_mount_point():
-    # Get a list of all storage devices and find the mount point of the first SD card
-    # Returns None if no SD card is found, otherwise returns the mount point
-
-    # Get list of storage devices using lsblk --json -o NAME,MOUNTPOINT
+    """
+    Get a list of all storage devices and find the mount point path of the first removable media
+    Input: None
+    Returns: None if no SD card is found, otherwise returns the mount point path as a string
+    """
+    
+    # Get list of storage devices and the partitions on them
+    # using terminal command `lsblk --json -o NAME,MOUNTPOINT`
     devices = os.popen("lsblk --json -o NAME,MOUNTPOINT").read()
     # Parse the JSON output
     devices = json.loads(devices)
     # Find the first device with a mount point and sd in the name
+    # when you mount any removable media (SD card/flash drive) onto Linux Pi, it will have
+    # a naming convention of "sd*" where * is some letter.
     for device in devices['blockdevices']:
         # Check if device is sd* and has children
+        # The children are the partitions on the drive
         if device['name'][:2] == "sd" and 'children' in device:
             # Check if any children have a mount point
             for child in device['children']:
@@ -41,8 +83,9 @@ def get_mount_point():
                     return child['mountpoint']
     return None
 
-
+# Main Thread
 if __name__ == '__main__':
+    # STATES
     power_led_time = 0
     power_led_state = GPIO.HIGH
     recording_led_time = 0
@@ -53,6 +96,7 @@ if __name__ == '__main__':
     recording_btn_time = 0
     mount_btn_time = 0
 
+    # GPIO PIN SETUP
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(POWER_LED_PIN, GPIO.OUT)
     GPIO.setup(RECORDING_LED_PIN, GPIO.OUT)
@@ -61,12 +105,15 @@ if __name__ == '__main__':
     GPIO.setup(RECORDING_BTN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(MOUNT_BTN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+    # Initialize status LEDs with their initial states
     GPIO.output(POWER_LED_PIN, power_led_state)
     GPIO.output(RECORDING_LED_PIN, recording_led_state)
     GPIO.output(MOUNT_LED_PIN, mount_led_state)
 
+    # Threading manager
     preview_queue = multiprocessing.Queue()
     manager = multiprocessing.Manager()
+
     # Use shared dictionary to store device state
     state = manager.dict()
     state['should_record'] = False
@@ -81,23 +128,23 @@ if __name__ == '__main__':
     state['web_heartbeat'] = False
     last_should_record = state['should_record']
 
-    last_cam_heartbeat = 0
-    last_web_heartbeat = 0
-
     # Start camera.camera_worker in a separate process
     camera_process = multiprocessing.Process(
-        target=camera_test.camera_worker, args=(PREVIEW_FRAMERATE, preview_queue, state,))
+        target=camera.camera_worker, args=(PREVIEW_FRAMERATE, preview_queue, state))
     camera_process.start()
     camera_worker_running = True
+
     # Start webui.web_worker in a separate process
     web_process = multiprocessing.Process(
-        target=webui.web_worker, args=(PREVIEW_FRAMERATE, preview_queue, state,))
+        target=webui.web_worker, args=(PREVIEW_FRAMERATE, preview_queue, state))
     web_process.start()
     web_worker_running = True
 
     print("Started worker processes")
 
+    # TODO check whether this variable is even used.
     mounted = False
+    # string to the path of the mount point directory
     mount_point = None
     print("Looking for mount point")
     while mount_point is None:
@@ -124,7 +171,7 @@ if __name__ == '__main__':
     if not os.path.exists(RECORDING_MAIN_DIRECTORY):
         os.makedirs(RECORDING_MAIN_DIRECTORY)
 
-    last_cam_heartbeat = time.time()
+    # MAIN WHILE LOOP
     while True:
         # if camera_worker_running and state['cam_heartbeat']:
         #     last_cam_heartbeat = time.time()
@@ -140,6 +187,7 @@ if __name__ == '__main__':
         #     state['recording_duration'] = 0
         #     state['error'] = "Camera worker process stopped unexpectedly, video segments may not have been merged. Please restart recording device."
 
+        # Control for Power Button
         if GPIO.input(POWER_BTN_PIN) == GPIO.LOW:
             if power_led_state == GPIO.HIGH:
                 state['should_record'] = False
@@ -153,11 +201,13 @@ if __name__ == '__main__':
                 GPIO.output(POWER_LED_PIN, GPIO.HIGH)
                 os.system("poweroff")
 
+        # Control for Recording Button
         if GPIO.input(RECORDING_BTN_PIN) == GPIO.LOW:
             if time.time() - recording_btn_time >= DEBOUNCE_TIME:
                 recording_btn_time = time.time()
                 state['should_record'] = not state['should_record']
 
+        # Control for Mount Button
         if GPIO.input(MOUNT_BTN_PIN) == GPIO.LOW:
             if time.time() - mount_btn_time >= DEBOUNCE_TIME:
                 mount_btn_time = time.time()
@@ -211,6 +261,7 @@ if __name__ == '__main__':
                 else:
                     recording_led_state = GPIO.HIGH
                 GPIO.output(RECORDING_LED_PIN, recording_led_state)
+        # Detected that recording must be started but we're not recording yet. Start up stuff
         if state['should_record'] == True and state['recording'] == False:
             # Make a new directory for this set of recordings
             os.makedirs(
@@ -227,6 +278,7 @@ if __name__ == '__main__':
             recording_led_state = GPIO.HIGH
             GPIO.output(RECORDING_LED_PIN, recording_led_state)
             state['recording_duration'] = 0
+        # Detected that recording must be stopped but we're still recording, clean up
         elif state['should_record'] == False and state['recording'] == True:
             for i in range(10):
                 if recording_led_state == GPIO.HIGH:
@@ -238,4 +290,5 @@ if __name__ == '__main__':
             recording_led_state = GPIO.LOW
             GPIO.output(RECORDING_LED_PIN, recording_led_state)
             state['recording_duration'] = 0
+            state['recording_directory'] = None
         time.sleep(0.1)
